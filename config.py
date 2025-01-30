a921fd0d2173dd509482ec971456315abcbd69a6
DefFoxPy <66576421+DefFoxPy@users.noreply.github.com>
2025-01-30 19:38:48 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv #debug
+
+load_dotenv() #debug
+TOKEN = os.environ['DISCORD_TOKEN']
+APPLICATION_ID = os.environ['APPLICATION_ID']
+CANAL_ID = 1170927738392154182
+#ID_SERVER = os.environ['SERVER_ID'] 
+
+LIMIT = 100 # limite de mensajes
